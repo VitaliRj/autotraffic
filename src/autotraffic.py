1eb5501f06964439cce3af78c0c46a694052ef90
Vitali RJ <vitali_r@mailbox.org>
2021-08-28 21:30:09 +0200

    added more api calls

--- src/autotraffic.py
+++ src/autotraffic.py
@@ -2,8 +2,6 @@ import requests
 import pandas as pd
 
 class Autobahn:
-    """ A Autobahn GmbH API client that returns easy to use DataFrames with information about the german Autobahns.
-    """
     def __init__(self, server="https://verkehr.autobahn.de/o/autobahn/"):
         self.server = server
 
@@ -12,8 +10,8 @@ class Autobahn:
         Args:
             None
         Returns:
-            [pandas.DataFrame] a pandas.DataFrame with available highways
-            if request is successful, else the None.
+            [pandas.DataFrame] if request is successful, a pandas.DataFrame with available highways
+            else the response.status_code.
         """
         response = requests.request("GET", self.server)
         if response.status_code == 200:
@@ -21,33 +19,17 @@ class Autobahn:
             
         else:
             print(response.status_code)
-            return None
+            return response.status_code
 
-    def get_webcams(self, road_id):
-        """ Returns a pandas.DataFrame with available webcams for a given road_id
+
+    # ----------- Road Works ----------- #
+    def get_roadworks(self, road_id):
+        """ Returns a pandas.DataFrame of the current construction for a given highway.
         Args:
             [string] road_id: a string refering to a highway e.g. 'A1'
         Returns:
-            [pandas.DataFrame] a pandas.DataFrame with available webcams
-            if request is successful, else the None.
-        """
-        url = f"https://verkehr.autobahn.de/o/autobahn/{road_id}/services/webcam"
-
-        response = requests.request("GET", url)
-
-        if response.status_code == 200:
-            return pd.DataFrame(response.json()['webcam'])
-        else:
-            print(response.status_code)
-            return None
-
-    def get_roadworks(self,road_id):
-        """ Returns a pandas.DataFrame of the current construction sites.
-        Args:
-            [string] road_id: a string refering to a highway e.g. 'A1'
-        Returns:
-            [pandas.DataFrame] a pandas.DataFrame with available webcams
-            if request is successful, else the None.
+            [pandas.DataFrame] if request is successful, a pandas.DataFrame with roadworks,
+            else the None.
         """
         url = f"https://verkehr.autobahn.de/o/autobahn/{road_id}/services/roadworks"
 
@@ -57,4 +39,142 @@ class Autobahn:
             return pd.DataFrame(response.json()['roadworks'])
         else:
             print(response.status_code)
-            return None
+            return response.status_code
+
+    def get_roadwork_details(self, roadwork_id):
+        """ Returns a details for a given roadwork identifier.
+        Args:
+            [string] roadwork_id: a string refering to a roadwork identifier
+        Returns:
+            [dict] if request is successful, a dict with details for a given roadwork_id,
+            else the response.status_code.
+        """
+        url = f"https://verkehr.autobahn.de/o/autobahn/details/roadworks/{roadwork_id}"
+
+        response = requests.request("GET", url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(response.status_code)
+            return response.status_code
+
+
+    # ----------- Webcams ----------- #
+    def get_webcams(self, road_id):
+        """ Returns a pandas.DataFrame with available webcams for a given highway.
+        Args:
+            [string] road_id: a string refering to a highway e.g. 'A1'
+        Returns:
+            [pandas.DataFrame] if request is successful, a pandas.DataFrame with available webcams,
+            else the response.status_code.
+        """
+        url = f"https://verkehr.autobahn.de/o/autobahn/{road_id}/services/webcam"
+
+        response = requests.request("GET", url)
+
+        if response.status_code == 200:
+            return pd.DataFrame(response.json()['webcam'])
+        else:
+            print(response.status_code)
+            return response.status_code
+
+    def get_webcams_details(self, webcam_id):
+        """ Returns a details for a given webcam identifier.
+        Args:
+            [string] webcam_id: a string refering to a webcam identifier
+        Returns:
+            [dict] if request is successful, a dict with details for a given webcam_id,
+            else the response.status_code.
+        """
+        url = f"https://verkehr.autobahn.de/o/autobahn/details/webcam/{webcam_id}"
+
+        response = requests.request("GET", url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(response.status_code)
+            return response.status_code
+
+
+    # ----------- Resting Areas -----------
+    def get_resting_areas(self, road_id):
+        """ Returns a pandas.DataFrame of possible resting areas along a given highway.
+        Args:
+            [string] road_id: a string refering to a highway e.g. 'A1'
+        Returns:
+            [pandas.DataFrame] if request is successful, a pandas.DataFrame with resting areas
+            along a given road_id, else the response.status_code.
+        """
+        url = f"https://verkehr.autobahn.de/o/autobahn/{road_id}/services/parking_lorry"
+
+        response = requests.request("GET", url)
+
+        if response.status_code == 200:
+            return pd.DataFrame(response.json()['parking_lorry'])
+        else:
+            print(response.status_code)
+            return response.status_code
+
+    def get_resting_area_details(self, rest_id):
+        """ Returns resting areas details for a given resting area identifier.
+        Args:
+            [string] road_id: a string refering to a highway e.g. 'A1'
+        Returns:
+            [dict] if request is successful, a dict with details for a given rest_id,
+            else the response.status_code.
+        """
+        url = f"https://verkehr.autobahn.de/o/autobahn/details/parking_lorry/{rest_id}"
+
+        response = requests.request("GET", url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(response.status_code)
+            return response.status_code
+
+
+    # ----------- Resting Areas ----------- #
+    def get_warnings(self, road_id):
+        """ Returns a pandas.DataFrame of current warnings for a given highway.
+        Args:
+            [string] road_id: a string refering to a highway e.g. 'A1'
+        Returns:
+            [pandas.DataFrame] if request is successful, a pandas.DataFrame with resting areas
+            along a given road_id, else the response.status_code.
+        """
+        url = f"https://verkehr.autobahn.de/o/autobahn/{road_id}/services/warning"
+
+        response = requests.request("GET", url)
+
+        if response.status_code == 200:
+            return pd.DataFrame(response.json()['warning'])
+        else:
+            print(response.status_code)
+            return response.status_code
+
+    def get_warning_details(self, warning_id):
+        """ Returns warning details for a given warning identifier.
+        Args:
+            [string] warning_id: a string refering to a highway e.g. 'A1'
+        Returns:
+            [dict] if request is successful, a dict with details for a given warning_id,
+            else the response.status_code.
+        """
+        url = f"https://verkehr.autobahn.de/o/autobahn/details/warning/{warning_id}"
+
+        response = requests.request("GET", url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(response.status_code)
+            return response.status_code
+
+
+    # ----------- Closures ----------- #
+    #Todo
+    # ----------- Charging Stations----------- #
+    #Todo
